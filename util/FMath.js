6bae5e43e9de16a3299b9de804b0d527f1131aa6
NovaSagittarii <novasagittarii@gmail.com>
2020-10-11 16:41:37 -0700

    rename FastTrig to FMath

--- util/FMath.js
+++ util/FMath.js
@@ -1,50 +1,50 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define(function () {
-            return (root.FastTrig = factory());
+            return (root.FMath = factory());
         });
     } else if (typeof module === 'object' && module.exports) {
         module.exports = factory();
     } else {
-        root.FastTrig = factory();
+        root.FMath = factory();
     }
 }(this, function () {
     var PI2 = Math.PI * 2;
 
-    FastTrig.DEFAULT_PARAMS = {
+    FMath.DEFAULT_PARAMS = {
         resolution: 360,
         minAtan: -40,
         maxAtan: 40
     };
-    
-    function FastTrig (params) {
-        this.params = FastTrig._assign(null, FastTrig.DEFAULT_PARAMS, params);
-        FastTrig._setDefaultValues(this.params);
+
+    function FMath (params) {
+        this.params = FMath._assign(null, FMath.DEFAULT_PARAMS, params);
+        FMath._setDefaultValues(this.params);
 
         this.cosTable = new Float32Array(this.params.nbCos);
         this.cosFactor = this.params.nbCos / PI2;
-        FastTrig._fillCache(this.cosTable, this.cosFactor, Math.cos);
+        FMath._fillCache(this.cosTable, this.cosFactor, Math.cos);
 
         this.sinTable = new Float32Array(this.params.nbSin);
         this.sinFactor = this.params.nbSin / PI2;
-        FastTrig._fillCache(this.sinTable, this.sinFactor, Math.sin);
+        FMath._fillCache(this.sinTable, this.sinFactor, Math.sin);
 
         this.atanTable = new Float32Array(this.params.nbAtan);
         this.atanFactor = this.params.nbAtan / (this.params.maxAtan - this.params.minAtan)
-        FastTrig._fillAtanCache(this.atanTable, this.atanFactor, this.params.minAtan);
+        FMath._fillAtanCache(this.atanTable, this.atanFactor, this.params.minAtan);
     };
 
-    FastTrig.prototype.cos = function (angle) {
+    FMath.prototype.cos = function (angle) {
         angle %= PI2;
         if (angle < 0) angle += PI2;
         return this.cosTable[(angle * this.cosFactor) | 0];
     };
-    FastTrig.prototype.sin = function (angle) {
+    FMath.prototype.sin = function (angle) {
         angle %= PI2;
         if (angle < 0) angle += PI2;
         return this.sinTable[(angle * this.sinFactor) | 0];
     };
-    FastTrig.prototype.atan = function (tan) {
+    FMath.prototype.atan = function (tan) {
         var index = ((tan - this.params.minAtan) * this.atanFactor) | 0;
         if (index < 0) {
             return - Math.PI / 2;
@@ -54,22 +54,22 @@
         return this.atanTable[index];
     };
     // What we learned in Math 3 Honors
-    FastTrig.prototype.atan2 = function (y, x) {
+    FMath.prototype.atan2 = function (y, x) {
         if (x > 0.0) {
             if (y === 0.0) return y;
-            if (x === Number.POSITIVE_INFINITY) return FastTrig._quadDef(y);
+            if (x === Number.POSITIVE_INFINITY) return FMath._quadDef(y);
             else return this.atan(y / x);
         } else if (x < 0.0) {
             if (y === 0.0) return Math.PI * Math.sign(y);
 
-            if (x === Number.NEGATIVE_INFINITY) return FastTrig._quadDef(y);
+            if (x === Number.NEGATIVE_INFINITY) return FMath._quadDef(y);
             else if (y > 0.0) return Math.PI/2 - this.atan(x / y);
             else if (y < 0.0) return -Math.PI/2 - this.atan(x / y);
             else return Number.NaN;
-        } else return FastTrig._quadDefZeroOrNan(y, x);
+        } else return FMath._quadDefZeroOrNan(y, x);
     };
 
-    FastTrig._quadDef = function (y) {
+    FMath._quadDef = function (y) {
         if (y === Number.POSITIVE_INFINITY) {
             return 3*Math.PI/4;
         } else if (y === Number.NEGATIVE_INFINITY) {
@@ -83,7 +83,7 @@
         }
     };
 
-    FastTrig._quadDefZeroOrNan = function (y, x) {
+    FMath._quadDefZeroOrNan = function (y, x) {
         if (x === 0.0) {
             if (y === 0.0) {
                 if (Math.sign(x) < 0) {
@@ -106,7 +106,7 @@
         }
     }
 
-    FastTrig._setDefaultValues = function (params) {
+    FMath._setDefaultValues = function (params) {
         var functionNames = ["nbSin", "nbCos", "nbAtan"];
         for (var i = functionNames.length - 1; i >= 0; i--) {
             var key = functionNames[i];
@@ -114,21 +114,21 @@
         }
     };
 
-    FastTrig._fillAtanCache = function (array, factor, min) {
+    FMath._fillAtanCache = function (array, factor, min) {
         for (var i = 0; i < array.length; i++) {
             var tan = min + i / factor;
             array[i] = Math.atan(tan);
         }
     };
 
-    FastTrig._fillCache = function (array, factor, mathFunction) {
+    FMath._fillCache = function (array, factor, mathFunction) {
         var length = array.length;
         for (var i = 0; i < length; i++) {
             array[i] = mathFunction(i / factor);
         }
     };
 
-    FastTrig._assign = function (dst, src1, src2, etc) {
+    FMath._assign = function (dst, src1, src2, etc) {
         return [].reduce.call(arguments, function (dst, src) {
             src = src || {};
             for (var k in src) {
@@ -140,5 +140,5 @@
         }, dst || {});
     };
 
-    return FastTrig;
+    return FMath;
 }));
